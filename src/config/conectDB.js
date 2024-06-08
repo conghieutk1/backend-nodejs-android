3738516c41f1a4694cd79f2d5d550cef791f0a3f
Hiếu Đặng <hieu37.nghiadung@gmail.com>
2024-06-08 19:35:40 +0700

    fix connect db

--- src/config/conectDB.js
+++ src/config/conectDB.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USE
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
+    dialectModule: require('pg'),
     logging: false,
     dialectOptions:
         process.env.DB_SSL === 'true'
